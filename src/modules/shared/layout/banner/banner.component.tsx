ae21068ffa355f91d6c02c83e987ce3e3b43bc66
Fernando Leiva <fleiva@folcode.com>
2024-03-02 18:23:46 -0300

    add controlls attribute

--- src/modules/shared/layout/banner/banner.component.tsx
+++ src/modules/shared/layout/banner/banner.component.tsx
@@ -5,20 +5,18 @@ import { AspectRatio } from '@mui/icons-material';
 function BannerComponent() {
     return (
         <div>
-            {/* <Box sx={{ position: 'relative', width: '100%', height: 0, maxHeight: 100, paddingTop: '56.25%' }}> */}
                 <Card sx={{ maxHeight: 500 }}> {/* Ajusta estas dimensiones según necesites */}
                     <CardMedia
                         component="video"
                         image="./banner.mp4" // Sustituye esto por la ruta de tu video
                         title="Video Banner"
                         style={{ height: '100%', width: '100%' }} // Asegura que el video llene el espacio del Card
-
+                        controls
                         autoPlay
                         loop
                         muted // Recomendado para no interferir con la experiencia del usuario
                     />
                 </Card>
-            {/* </Box> */}
         </div>
     );
 }
